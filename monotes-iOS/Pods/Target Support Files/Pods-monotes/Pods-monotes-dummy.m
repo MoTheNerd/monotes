5b8fe0d782ce90393cddaf83fb293a4ea5bf6248
Mohammad Al-Ahdal <mkaa00x@live.co.uk>
2017-03-27 09:13:13 -0600

    Add Swift Project for iOS Devices

---
+++ monotes-iOS/Pods/Target Support Files/Pods-monotes/Pods-monotes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_monotes : NSObject
+@end
+@implementation PodsDummy_Pods_monotes
+@end
---
+++ monotes-iOS/monotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  monotes
+//
+//  Created by Mohammad Al-Ahdal on 2017-03-27.
+//  Copyright © 2017 Mohammad Al-Ahdal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
